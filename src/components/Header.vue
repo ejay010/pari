e41f56f2199c46e85554367bc7685defaf8f5925
ejay010 <ejay010@gmail.com>
2019-04-15 19:11:11 -0400

    Navbar update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,22 @@
 <template>
 <!-- Header -->
 <header class="masthead">
-  <div class="container d-flex h-100 align-items-center">
+  <div class="container d-flex align-items-center" id="headercontainer">
     <div class="mx-auto text-center">
-      <h1 class="mx-auto my-0 text-uppercase">P-ARI Foundation</h1>
-      <h2 class="text-white-50 mx-auto mt-2 mb-5">P<span class="color1">AN</span>-A<span class="color1">FRICAN</span> R<span class="color1">ENAISSANCE</span> I<span class="color1">NITIATIVE</span></h2>
+      <h1 class="mx-auto my-0 text-uppercase" id="pagetitle">P-ARI Foundation</h1>
+      <h2 class="text-white-50 mx-auto mt-2 mb-2">P<span class="color1">AN</span>-A<span class="color1">FRICAN</span> R<span class="color1">ENAISSANCE</span> I<span class="color1">NITIATIVE</span></h2>
       <a href="#about" class="btn btn-primary js-scroll-trigger">Let's Learn from the west to develop the best</a>
     </div>
   </div>
 </header>
 </template>
+
+<style>
+#headercontainer {
+  height: 55% !important;
+}
+
+#pagetitle {
+  letter-spacing: 0.1rem !important;
+}
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -13,10 +13,19 @@
           <a class="nav-link js-scroll-trigger" href="#about">About</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">Pan-Africa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">Missions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">Services</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
+          <a class="nav-link js-scroll-trigger" href="#signup">Opportunities</a>
         </li>
       </ul>
     </div>
